579b5da14b150f92b1757b7e9003d86c603646c7
Minah7 <105715484+Minah7@users.noreply.github.com>
2023-07-18 00:08:16 +0900

    A 강조하기 23.07.18

---
+++ Q0055.java
@@ -0,0 +1,12 @@
+// 문자열 myString이 주어집니다. myString에서 알파벳 "a"가 등장하면 전부 "A"로 변환하고, 
+// "A"가 아닌 모든 대문자 알파벳은 소문자 알파벳으로 변환하여 return 하는 solution 함수를 완성하세요.
+
+class Solution {
+    public String solution(String myString) {
+        String a = myString.toLowerCase();
+        String answer = a.replaceAll("a","A");
+        return answer;
+    }
+}
+
+//성공
