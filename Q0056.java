09ad07e5f3d8a9c73deace36bdf46f3b0be9c0a8
Minah7 <105715484+Minah7@users.noreply.github.com>
2023-07-18 00:19:43 +0900

    문자열 바꿔서 찾기 23.07.17

---
+++ Q0056.java
@@ -0,0 +1,28 @@
+// 문자 "A"와 "B"로 이루어진 문자열 myString과 pat가 주어집니다. 
+// myString의 "A"를 "B"로, "B"를 "A"로 바꾼 문자열의 연속하는 부분 문자열 중 
+// pat이 있으면 1을 아니면 0을 return 하는 solution 함수를 완성하세요.
+
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        String AB = "";
+        
+        for(int i = 0; i < myString.length(); i++) {
+            char c = myString.charAt(i);
+            if(c == 'A') {
+                AB += 'B';
+            } else {
+                AB += 'A';
+            }
+        }
+        
+        if(AB.contains(pat)) {
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+        return answer;
+    }
+}
+
+//성공
