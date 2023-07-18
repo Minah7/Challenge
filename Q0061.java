227333f7246044d9e78e7d17edbcbad9739670f2
Minah7 <105715484+Minah7@users.noreply.github.com>
2023-07-18 22:09:11 +0900

    정수 찾기 23.07.18

---
+++ Q0061.java
@@ -0,0 +1,19 @@
+// 정수 리스트 num_list와 찾으려는 정수 n이 주어질 때, 
+// num_list안에 n이 있으면 1을 없으면 0을 return하도록 solution 함수를 완성해주세요.
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = 0;
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] == n) {
+                answer = 1;
+                break;
+            } else {
+                answer = 0;
+            }
+        }
+        return answer;
+    }
+}
+
+//성공
