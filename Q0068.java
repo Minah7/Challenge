309cc2bed3172608a92d8c88b6cf9ee2da5cf363
Minah7 <105715484+Minah7@users.noreply.github.com>
2023-07-19 00:48:13 +0900

    Q0068.java
    Update 배열의 원소만큼 추가하기
    실패했다가 성공
    23.07.19

--- Q0068.java
+++ Q0068.java
@@ -2,16 +2,18 @@
 // 양의 정수 배열 arr가 매개변수로 주어질 때, arr의 앞에서부터 차례대로 원소를 보면서 
 // 원소가 a라면 X의 맨 뒤에 a를 a번 추가하는 일을 반복한 뒤의 배열 X를 return 하는 solution 함수를 작성해 주세요.
 
+import java.util.*;
+
 class Solution {
     public List solution(int[] arr) {
         List<Integer> answer = new ArrayList<>();
         for (int i = 0; i< arr.length; i++) {
             for (int j = 0; j< arr[i]; j++) {
-                list.add(arr[i]);
+                answer.add(arr[i]);
             }
         }
         return answer;
     }
 }
 
-//실패. 
+//실패. 했다가 성공 
