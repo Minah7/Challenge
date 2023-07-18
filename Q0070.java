1e6c67b1f3838af9cb791a803f7a8f6589359355
Minah7 <105715484+Minah7@users.noreply.github.com>
2023-07-19 00:39:08 +0900

    접미사인지 확인하기 23.07.19

---
+++ Q0070.java
@@ -0,0 +1,15 @@
+// 어떤 문자열에 대해서 접미사는 특정 인덱스부터 시작하는 문자열을 의미합니다. 
+// 예를 들어, "banana"의 모든 접미사는 "banana", "anana", "nana", "ana", "na", "a"입니다.
+// 문자열 my_string과 is_suffix가 주어질 때, is_suffix가 my_string의 접미사라면 1을, 아니면 0을 return 하는 solution 함수를 작성해 주세요.
+
+class Solution {
+    public int solution(String myString, String isSuffix) {
+        int answer = 0;
+        if (myString.endsWith(isSuffix)) {
+            answer = 1;
+        }
+        return answer;
+    }
+}
+
+//성공
