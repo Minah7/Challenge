c8353d9343f3f00b9e76c95ddd5f27cded44e19b
Minah7 <105715484+Minah7@users.noreply.github.com>
2023-07-20 22:07:32 +0900

    특별한 이차원 배열 2 23.07.20

---
+++ Q0084.java
@@ -0,0 +1,29 @@
+// n × n 크기의 이차원 배열 arr이 매개변수로 주어질 때, arr이 다음을 만족하면 1을 아니라면 0을 return 하는 solution 함수를 작성해 주세요.
+//0 ≤ i, j < n인 정수 i, j에 대하여 arr[i][j] = arr[j][i]
+
+class Solution {
+    public int solution(int[][] arr) {
+        int answer = 1;
+        int n = arr.length;
+        
+        for (int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                if(arr[i][j]!=arr[j][i]) {
+                    answer = 0;
+                }
+            }
+        }
+        return answer;
+    }
+}
+
+//성공.
+for (int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                if(arr[i][j]==arr[j][i]) {
+                    answer = 1;
+                }
+            }
+        }
+
+//이렇게 하면 왜 안될까..
