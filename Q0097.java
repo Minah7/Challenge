50d94ca04497af5d18208c8abe4b835749d454ea
Minah7 <105715484+Minah7@users.noreply.github.com>
2023-07-22 01:28:08 +0900

    리스트 자르기 23.07.21

---
+++ Q0097.java
@@ -0,0 +1,70 @@
+// 정수 n과 정수 3개가 담긴 리스트 slicer 그리고 정수 여러 개가 담긴 리스트 num_list가 주어집니다. slicer에 담긴 정수를 차례대로 a, b, c라고 할 때, n에 따라 다음과 같이 num_list를 슬라이싱 하려고 합니다.
+// n = 1 : num_list의 0번 인덱스부터 b번 인덱스까지
+// n = 2 : num_list의 a번 인덱스부터 마지막 인덱스까지
+// n = 3 : num_list의 a번 인덱스부터 b번 인덱스까지
+// n = 4 : num_list의 a번 인덱스부터 b번 인덱스까지 c 간격으로
+// 올바르게 슬라이싱한 리스트를 return하도록 solution 함수를 완성해주세요.
+
+//두번째로 실패한 코드. 19개의 테스트 중 4개의 테스트에서 런타임 에러.
+import java.util.*;
+
+class Solution {
+    public List solution(int n, int[] slicer, int[] numList) {
+        List<Integer> answer = new ArrayList<>();
+        int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+        
+        if (n == 1) {
+            for (int i = 0; i <= b; i++) {
+                answer.add(numList[i]);
+            }
+        } else if (n == 2) {
+            for (int i = a; i <= numList.length; i++) {
+                answer.add(numList[i]);
+            }
+        } else if (n == 3) {
+            for (int i = a; i <= b; i++) {
+                answer.add(numList[i]);
+            }
+        } else if (n == 4) {
+            for (int i = a; i <= b; i+=c) {
+                answer.add(numList[i]);
+            }
+        }
+        return answer;
+    }
+}
+
+// 처음 작성한 코드. n=4에서 c간격 때문에 수정. 실패.
+import java.util.*;
+
+class Solution {
+    public List solution(int n, int[] slicer, int[] numList) {
+        List<Integer> answer = new ArrayList<>();
+        int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+        
+        for(int i = 0; i < numList.length; i++) {
+            if (n == 1) {
+                if (i <= b) {
+                    answer.add(numList[i]);
+                }
+            } else if (n == 2) {
+                if (i >= a) {
+                    answer.add(numList[i]);
+                }
+            } else if (n == 3) {
+                if (i >= a && i <= b) {
+                    answer.add(numList[i]);
+                }
+            } else if (n == 4) {
+                if (i >= a && i <= b) {
+                    
+                }
+            }
+        }
+        return answer;
+    }
+}
