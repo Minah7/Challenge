1edeeac917ccc524bb619e0f45a395fb177945d3
Minah7 <105715484+Minah7@users.noreply.github.com>
2023-07-22 23:48:17 +0900

    0 떼기 23.07.22

---
+++ Q0106.java
@@ -0,0 +1,20 @@
+// 정수로 이루어진 문자열 n_str이 주어질 때, n_str의 가장 왼쪽에 처음으로 등장하는 0들을 뗀 문자열을 return하도록 solution 함수를 완성해주세요.
+
+class Solution {
+    public String solution(String nStr) {
+        String answer = "";
+        for (int i = 0; i < nStr.length(); i++) {
+            if (nStr.charAt(i) == '0') {
+                continue;
+            } else {
+                for (int j = i; j < nStr.length(); j++) {
+                    answer += nStr.charAt(j);
+                }
+            break;
+            }
+        }
+        return answer;
+    }
+}
+
+//성공
