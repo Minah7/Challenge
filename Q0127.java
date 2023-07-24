b8af442e46c95e886e5c3927002a091a1acbf520
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-07-25 00:12:44 +0900

    두 수의 합 23.07.24

---
+++ Q0127.java
@@ -0,0 +1,10 @@
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+// 성공
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1+num2;
+        return answer;
+    }
+}
