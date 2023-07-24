4c107c8b4f18891b28544cabaccdf60e7836117d
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-07-25 00:14:11 +0900

    나이 출력 23.07.24

---
+++ Q0128.java
@@ -0,0 +1,11 @@
+// 머쓱이는 40살인 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 
+// 나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
+
+// 성공
+
+class Solution {
+    public int solution(int age) {
+        int answer = 2022-age+1;
+        return answer;
+    }
+}
