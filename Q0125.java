215fc9b7da657ff6bef40c18154f3ba7a49fb221
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-07-25 00:08:30 +0900

    나머지 구하기 23.07.24

---
+++ Q0125.java
@@ -0,0 +1,10 @@
+// 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+
+// 성공
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1%num2;
+        return answer;
+    }
+}
