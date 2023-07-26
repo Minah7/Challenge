434e5bb4b4ba8033f0fd69d900ba58b627ddb47d
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-07-26 16:20:14 +0900

    수열과 구간 쿼리 4 23.07.26

---
+++ Q0142.java
@@ -0,0 +1,29 @@
+// 정수 배열 arr와 2차원 정수 배열 queries이 주어집니다. 
+// queries의 원소는 각각 하나의 query를 나타내며, [s, e, k] 꼴입니다.
+// 각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 i가 k의 배수이면 arr[i]에 1을 더합니다.
+// 위 규칙에 따라 queries를 처리한 이후의 arr를 return 하는 solution 함수를 완성해 주세요.
+
+// 성공
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[arr.length];
+        for(int i = 0; i < arr.length; i++) {
+            answer[i] = arr[i];
+        }
+        
+        for(int j = 0; j < queries.length; j++) {
+            int s = queries[j][0];
+            int e = queries[j][1];
+            int k = queries[j][2];
+            for(int l = s; l <= e; l++) {
+                if(l%k == 0) {
+                    answer[l] += 1;
+                }
+            }
+        }
+        return answer;
+    }
+}
