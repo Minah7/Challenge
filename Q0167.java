b2811161bd8cf4e8a86d319f6b3c341235537d8a
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-07-28 21:29:08 +0900

    모음 제거 23.07.28

---
+++ Q0167.java
@@ -0,0 +1,16 @@
+영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다. 
+문자열 my_string이 매개변수로 주어질 때 모음을 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+성공
+
+class Solution {
+    public String solution(String myString) {
+        String[] replace = {"a", "e", "i", "o", "u"};
+        for(int i = 0; i < replace.length; i++) {
+            if(myString.contains(replace[i])) {
+                myString = myString.replaceAll(replace[i], "");
+            }
+        }
+        return myString;
+    }
+}
