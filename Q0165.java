b10d43c1035046ca3bbb4e54271ee54addd2d280
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-07-28 21:02:36 +0900

    배열의 유사도 23.07.28

---
+++ Q0165.java
@@ -0,0 +1,18 @@
+두 배열이 얼마나 유사한지 확인해보려고 합니다. 
+문자열 배열 s1과 s2가 주어질 때 같은 원소의 개수를 return하도록 solution 함수를 완성해주세요.
+
+성공
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for(int i = 0; i < s1.length; i++) {
+            for(int j = 0; j < s2.length; j++) {
+                if(s1[i].equals(s2[j])) {
+                    answer += 1;
+                }
+            }
+        }
+        return answer;
+    }
+}
