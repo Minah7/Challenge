652f1af9dc4793e891bb6619150a8b982345c9d4
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-07-31 23:29:01 +0900

    공배수 23.07.31

---
+++ Q0182.java
@@ -0,0 +1,16 @@
+// 정수 number와 n, m이 주어집니다. 
+// number가 n의 배수이면서 m의 배수이면 1을 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+
+// 성공
+
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        if(number%n == 0 && number%m == 0) {
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+        return answer;
+    }
+}
