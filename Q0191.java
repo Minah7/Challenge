b8a8b8297d63b1cb389b3cb8dd5a1b4a8caf5832
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-01 00:42:31 +0900

    공백으로 구분하기 1 23.08.01

---
+++ Q0191.java
@@ -0,0 +1,11 @@
+단어가 공백 한 개로 구분되어 있는 문자열 my_string이 매개변수로 주어질 때, 
+my_string에 나온 단어를 앞에서부터 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+
+성공
+
+class Solution {
+    public String[] solution(String myString) {
+        String[] answer = myString.split(" ");
+        return answer;
+    }
+}
