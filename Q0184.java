a5583a2ec56fae34d318ae2fb79b8051643998f4
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-07-31 23:46:00 +0900

    정수 부분 23.07.31

---
+++ Q0184.java
@@ -0,0 +1,11 @@
+// 실수 flo가 매개 변수로 주어질 때, 
+// flo의 정수 부분을 return하도록 solution 함수를 완성해주세요.
+
+// 성공
+
+class Solution {
+    public int solution(double flo) {
+        int answer = (int)flo;
+        return answer;
+    }
+}
