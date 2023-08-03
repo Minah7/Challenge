3d11814aa7b2df572c62d86c45e2c650d1317102
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-04 01:04:22 +0900

    문자열안에 문자열 23.08.03

---
+++ Q0229.java
@@ -0,0 +1,14 @@
+문자열 str1, str2가 매개변수로 주어집니다. 
+str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
+
+성공
+
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 2;
+        if(str1.contains(str2)) {
+            answer = 1;
+        }
+        return answer;
+    }
+}
