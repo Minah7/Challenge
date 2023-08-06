e5a4bc1906992c7732e554887b04c4bfa848320c
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-06 19:44:47 +0900

    709. To Lower Case 23.08.06

---
+++ Q0254.java
@@ -0,0 +1,10 @@
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+Accepted
+
+class Solution {
+    public String toLowerCase(String s) {
+        s = s.toLowerCase();
+        return s;
+    }
+}
