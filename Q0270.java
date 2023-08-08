793de40db4cba16415a3f10e447bef58e43eab35
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-09 07:51:36 +0900

    직각삼각형 출력하기 23.08.09

---
+++ Q0270.java
@@ -0,0 +1,20 @@
+// "*"의 높이와 너비를 1이라고 했을 때, 
+// "*"을 이용해 직각 이등변 삼각형을 그리려고합니다. 
+// 정수 n 이 주어지면 높이와 너비가 n 인 직각 이등변 삼각형을 출력하도록 코드를 작성해보세요.
+
+// 성공
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
