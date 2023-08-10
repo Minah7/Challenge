160c7c3b005b1b40098f391a2c9c60d44abce613
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-10 22:01:46 +0900

    2071. 평균값 구하기 23.08.10

---
+++ Q0285.java
@@ -0,0 +1,23 @@
+// 10개의 수를 입력 받아, 평균값을 출력하는 프로그램을 작성하라.
+// (소수점 첫째 자리에서 반올림한 정수를 출력한다.)
+
+// 성공
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int tc = 1; tc <= T; tc++) {
+            double sum = 0;
+            for (int i = 0; i < 10; i++) {
+                int n = sc.nextInt();
+                sum += n;
+            }
+            double avg = sum/10;
+            System.out.println("#"+tc+" "+Math.round(avg));
+		}
+	}
+}
