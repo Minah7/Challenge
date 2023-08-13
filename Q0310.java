1f599b5d663d2bdbe5e397940f20e1ea578a4b04
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-13 23:24:37 +0900

    9로 나눈 나머지 23.08.13

---
+++ Q0310.java
@@ -0,0 +1,11 @@
+// 음이 아닌 정수를 9로 나눈 나머지는 그 정수의 각 자리 숫자의 합을 9로 나눈 나머지와 같은 것이 알려져 있습니다.
+// 이 사실을 이용하여 음이 아닌 정수가 문자열 number로 주어질 때, 이 정수를 9로 나눈 나머지를 return 하는 solution 함수를 작성해주세요.
+
+// 실패
+
+class Solution {
+    public int solution(String number) {
+        int answer = Integer.valueOf(number)%9;
+        return answer;
+    }
+}
