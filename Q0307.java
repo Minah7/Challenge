9ce0e624950ed11959f45c093da5dbdf9d3a905b
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-13 23:12:12 +0900

    a와 b 출력하기 23.08.13

---
+++ Q0307.java
@@ -0,0 +1,16 @@
+// 정수 a와 b가 주어집니다. 
+// 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+// 성공
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }
+}
