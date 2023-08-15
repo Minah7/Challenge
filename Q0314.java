1afcdd153bf1caced4fdfdda779db598853de0e0
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-15 21:07:06 +0900

    자릿수 더하기 23.08.15

---
+++ Q0314.java
@@ -0,0 +1,19 @@
+// 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+// 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+
+// 성공
+
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String N = String.valueOf(n);
+        for(int i = 0; i < N.length(); i++) {
+            int num = N.charAt(i) - 48;
+            answer += num;
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
