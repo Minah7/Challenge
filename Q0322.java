f5cad20c755a99713c6d7f7f545a46bef87d008c
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-15 22:08:25 +0900

    정수 내림차순으로 배치하기 23.08.15

---
+++ Q0322.java
@@ -0,0 +1,21 @@
+// 함수 solution은 정수 n을 매개변수로 입력받습니다. 
+// n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 
+// 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+
+// 실패
+
+import java.util.*;
+class Solution {
+    public long solution(long n) {
+        String N = Long.toString(n);
+        Integer[] nums = new Integer[N.length()];
+        for(int i = 0; i < N.length(); i++) {
+            nums[i] = N.charAt(i) - 48;
+        }
+        Arrays.sort(nums, Collections.reverseOrder());
+        String ans = Arrays.toString(nums);
+        long answer = Integer.valueOf(ans);
+        return answer;
+        // Long.parseLong(nums.toString())
+    }
+}
