1a1e670f80ef4225447213d06ae85aa30899f657
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-15 21:18:41 +0900

    x만큼 간격이 있는 n개의 숫자 23.08.15

---
+++ Q0316.java
@@ -0,0 +1,15 @@
+// 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 
+// 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+// 성공
+
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        for(int i = 1; i <= n; i++) {
+            long X = x;
+            answer[i-1] = X*i;
+        }
+        return answer;
+    }
+}
