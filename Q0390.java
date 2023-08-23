550c30b18c79c8ff56579ef8989b1af23cbd97ed
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-23 23:40:47 +0900

    qr code 23.08.23

---
+++ Q0390.java
@@ -0,0 +1,15 @@
+// 두 정수 q, r과 문자열 code가 주어질 때, code의 각 인덱스를 q로 나누었을 때 나머지가 r인 위치의 문자를 앞에서부터 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+// 성공
+
+class Solution {
+    public String solution(int q, int r, String code) {
+        String answer = "";
+        for(int i = 0; i < code.length(); i+=q) {
+            if(i+r < code.length()) {
+                answer += code.charAt(i+r);
+            }
+        }
+        return answer;
+    }
+}
