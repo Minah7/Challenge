745e187140f40dd3e619fb2d4f613bbc5c9b7280
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-24 23:16:29 +0900

    배열 만들기 1 23.08.24

---
+++ Q0400.java
@@ -0,0 +1,13 @@
+// 정수 n과 k가 주어졌을 때, 1 이상 n이하의 정수 중에서 k의 배수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+
+// 성공
+
+class Solution {
+    public int[] solution(int n, int k) {
+        int[] answer = new int[n/k];
+        for(int i = k, j = 0; i <= n; i+=k) {
+            answer[j++] = i;
+        } 
+        return answer;
+    }
+}
