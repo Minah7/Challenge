a166b25f3584351b164b74ce4c2b397f38e21d82
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-24 23:20:10 +0900

    꼬리 문자열 23.08.24

---
+++ Q0401.java
@@ -0,0 +1,16 @@
+// 문자열들이 담긴 리스트가 주어졌을 때, 모든 문자열들을 순서대로 합친 문자열을 꼬리 문자열이라고 합니다. 꼬리 문자열을 만들 때 특정 문자열을 포함한 문자열은 제외시키려고 합니다. 예를 들어 문자열 리스트 ["abc", "def", "ghi"]가 있고 문자열 "ef"를 포함한 문자열은 제외하고 꼬리 문자열을 만들면 "abcghi"가 됩니다.
+// 문자열 리스트 str_list와 제외하려는 문자열 ex가 주어질 때, str_list에서 ex를 포함한 문자열을 제외하고 만든 꼬리 문자열을 return하도록 solution 함수를 완성해주세요.
+
+// 성공
+
+class Solution {
+    public String solution(String[] strList, String ex) {
+        String answer = "";
+        for(int i = 0; i < strList.length; i++) {
+            if(!strList[i].contains(ex)) {
+                answer += strList[i];
+            }
+        }
+        return answer;
+    }
+}
