5cd4ca05fd8ce429da62c68310c322c096838fba
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-28 22:47:24 +0900

    직사각형 별찍기 23.08.28

---
+++ Q0442.java
@@ -0,0 +1,20 @@
+// 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+// 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+
+// 성공
+
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int r = 0; r < b; r++) {
+            for(int c = 0; c < a; c++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
