3f4ff944da6802841f1d9d0dad261fe99996b5dd
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-31 21:46:11 +0900

    rny_string 23.08.31

---
+++ Q0471.java
@@ -0,0 +1,11 @@
+// 'm'과 "rn"이 모양이 비슷하게 생긴 점을 활용해 문자열에 장난을 하려고 합니다. 
+// 문자열 rny_string이 주어질 때, rny_string의 모든 'm'을 "rn"으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+// 성공
+
+class Solution {
+    public String solution(String rnyString) {
+        String answer = rnyString.replace("m", "rn");
+        return answer;
+    }
+}
