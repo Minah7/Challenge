9d5a79cfb2994093a36d49a88a038d1707c003a9
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-08-31 21:48:59 +0900

    제곱수 판별하기 23.08.31

---
+++ Q0472.java
@@ -0,0 +1,17 @@
+// 어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 
+// 정수 n이 매개변수로 주어질 때, n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
+
+// 성공
+
+class Solution {
+    public int solution(int n) {
+        int answer = 2;
+        for(int i = 1; i < n; i++) {
+            if(i*i == n) {
+                answer = 1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
