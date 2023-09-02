b9e972186c6e8a9297dd801e7a3a922a8a6af3b1
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-02 23:45:41 +0900

    콜라 문제 23.09.02

--- Q0483.java
+++ Q0483.java
@@ -6,7 +6,7 @@
 // 콜라를 받기 위해 마트에 주어야 하는 병 수 a, 빈 병 a개를 가져다 주면 마트가 주는 콜라 병 수 b, 상빈이가 가지고 있는 빈 병의 개수 n이 매개변수로 주어집니다. 
 // 상빈이가 받을 수 있는 콜라의 병 수를 return 하도록 solution 함수를 작성해주세요.
 
-// 성공
+// 제출 실패
 
 class Solution {
     public int solution(int a, int b, int n) {
