5ae47608caa142feccabc22d7f34cb07f7c2a36e
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-03 23:27:29 +0900

    나머지가 1이 되는 수 찾기 23.09.03

---
+++ Q0490.java
@@ -0,0 +1,17 @@
+// 자연수 n이 매개변수로 주어집니다. 
+// n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요. 답이 항상 존재함은 증명될 수 있습니다.
+
+// 성공
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int x = 2; x < n; x++) {
+            if(n%x == 1) {
+                answer = x;
+                break;
+            }
+        }
+        return answer;
+    }
+}
