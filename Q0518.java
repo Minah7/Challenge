5c7c398e0691a465682876261a67635a36612ba3
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-06 00:21:42 +0900

    수박수박수박수박수박수? 23.09.05

---
+++ Q0518.java
@@ -0,0 +1,18 @@
+// 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 
+// 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+
+// 성공
+
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        for(int i = 0; i < n; i++) {
+            if(i%2== 0) {
+                answer += "수";
+            } else {
+                answer += "박";
+            }
+        }
+        return answer;
+    }
+}
