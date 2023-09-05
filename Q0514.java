6ec3841a97549b03f487048e44d50bb9860a5bf6
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-05 23:38:32 +0900

    다항식 더하기 23.09.05

--- Q0514.java
+++ Q0514.java
@@ -3,7 +3,7 @@
 // 덧셈으로 이루어진 다항식 polynomial이 매개변수로 주어질 때, 동류항끼리 더한 결괏값을 문자열로 return 하도록 solution 함수를 완성해보세요. 
 // 같은 식이라면 가장 짧은 수식을 return 합니다.
 
-// 성공
+// 실패
 
 class Solution {
     public String solution(String polynomial) {
