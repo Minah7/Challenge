230f17799ff605cb9662f703f1328cc49ae094ae
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-06 23:47:09 +0900

    간단한 논리 연산 23.09.06

---
+++ Q0529.java
@@ -0,0 +1,10 @@
+boolean 변수 x1, x2, x3, x4가 매개변수로 주어질 때, 다음의 식의 true/false를 return 하는 solution 함수를 작성해 주세요.
+
+(x1 ∨ x2) ∧ (x3 ∨ x4)
+
+class Solution {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean answer = (x1||x2)&&(x3||x4);
+        return answer;
+    }
+}
