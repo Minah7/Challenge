37291e2f45e1e84be39616fc60b1b622429ee0d4
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-07 17:41:22 +0900

    배열의 길이에 따라 다른 연산하기 23.09.07

---
+++ Q0532.java
@@ -0,0 +1,22 @@
+// 정수 배열 arr과 정수 n이 매개변수로 주어집니다. 
+// arr의 길이가 홀수라면 arr의 모든 짝수 인덱스 위치에 n을 더한 배열을, 
+// arr의 길이가 짝수라면 arr의 모든 홀수 인덱스 위치에 n을 더한 배열을 return 하는 solution 함수를 작성해 주세요.
+
+// 성공
+
+class Solution {
+    public int[] solution(int[] arr, int n) {
+        for(int i = 0; i < arr.length; i++) {
+            if(arr.length%2 == 1) {
+                if(i%2 == 0) {
+                    arr[i] += n;
+                }
+            } else {
+                if(i%2 == 1) {
+                    arr[i] += n;
+                }
+            }
+        }
+        return arr;
+    }
+}
