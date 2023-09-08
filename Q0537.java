1407ab22a322a8f3ef9df2755de668d0eb610b97
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-08 23:16:56 +0900

    분수의 덧셈 23.09.08

---
+++ Q0537.java
@@ -0,0 +1,23 @@
+// 첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1, 
+// 두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다. 
+// 두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+// 성공
+
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int numerator = (numer1*denom2) + (numer2*denom1);
+        int denominator = denom1 * denom2;
+        int min = Math.min(numerator, denominator);
+        for(int n = 2; n <= min; n++) {
+            if(numerator%n == 0 && denominator%n == 0) {
+                while(numerator%n == 0&& denominator%n == 0) {
+                    numerator /= n;
+                    denominator /= n;   
+                }
+            }
+        }
+        int[] answer = {numerator, denominator};
+        return answer;
+    }
+}
