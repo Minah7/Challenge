e6f165d038693475ff90c6adb7049c64a012bcd3
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-08 23:46:03 +0900

    그림 확대 23.09.08

---
+++ Q0539.java
@@ -0,0 +1,24 @@
+직사각형 형태의 그림 파일이 있고, 
+이 그림 파일은 1 × 1 크기의 정사각형 크기의 픽셀로 이루어져 있습니다. 
+이 그림 파일을 나타낸 문자열 배열 picture과 정수 k가 매개변수로 주어질 때, 
+이 그림 파일을 가로 세로로 k배 늘린 그림 파일을 나타내도록 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+
+성공
+
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length*k];
+        for(int i = 0, idx = 0; i < picture.length; i++) {
+            for(int row = 0; row < k; row++) {
+                String tmp = "";
+                for(int ch = 0; ch < picture[i].length(); ch++) {
+                    for(int repeat = 0; repeat < k; repeat++) {
+                        tmp += picture[i].charAt(ch);
+                    }
+                }
+                answer[idx++] = tmp;
+            }
+        }
+        return answer;
+    }
+}
