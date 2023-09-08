121799bdd1c3cb8b4d410772f3d49ff781409afe
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-08 22:56:16 +0900

    연속된 수의 합 23.09.08

---
+++ Q0536.java
@@ -0,0 +1,24 @@
+// 연속된 세 개의 정수를 더해 12가 되는 경우는 3, 4, 5입니다. 
+// 두 정수 num과 total이 주어집니다. 
+// 연속된 수 num개를 더한 값이 total이 될 때, 정수 배열을 오름차순으로 담아 return하도록 solution함수를 완성해보세요.
+
+// 성공
+
+class Solution {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        for(int n = -100; n < 1000; n++) {
+            int sum = 0;
+            for(int i = 0; i < num; i++) {
+                sum+= n+i;
+            }
+            if(sum == total) {
+                for(int i = 0; i < num; i++) {
+                    answer[i] = n+i;
+                }
+                break;
+            }
+        }
+        return answer;
+    }
+}
