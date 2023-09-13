b7b921862b8daf944d6da07f8b11a3a3ae571b19
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-14 01:46:51 +0900

    배열 원소의 길이 23.09.14

---
+++ Q0584.java
@@ -0,0 +1,14 @@
+// 문자열 배열 strlist가 매개변수로 주어집니다. 
+// strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
+
+// 성공
+
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = strlist[i].length();
+        }
+        return answer;
+    }
+}
