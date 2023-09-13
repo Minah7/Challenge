3e8b8182174e9b596068cfa08d063aa12a6b9ba9
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-14 01:50:05 +0900

    특정 문자 제거하기 23.09.14

---
+++ Q0586.java
@@ -0,0 +1,11 @@
+// 문자열 my_string과 문자 letter이 매개변수로 주어집니다. 
+// my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+// 성공
+
+class Solution {
+    public String solution(String myString, String letter) {
+        String answer = myString.replaceAll(letter, "");
+        return answer;
+    }
+}
