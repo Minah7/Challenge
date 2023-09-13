73dd2c0f1b80c525194c4e4bf852d8312c601678
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-14 01:45:02 +0900

    양꼬치 23.09.14

---
+++ Q0583.java
@@ -0,0 +1,12 @@
+// 머쓱이네 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스로 줍니다. 
+// 양꼬치는 1인분에 12,000원, 음료수는 2,000원입니다. 
+// 정수 n과 k가 매개변수로 주어졌을 때, 양꼬치 n인분과 음료수 k개를 먹었다면 총얼마를 지불해야 하는지 return 하도록 solution 함수를 완성해보세요.
+
+// 성공
+
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 12000*n + 2000*(k-n/10);
+        return answer;
+    }
+}
