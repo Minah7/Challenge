8e94087030abb6892386f5a7b6bd02734228fbed
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-15 00:58:25 +0900

    flag에 따라 다른 값 반환하기 23.09.15

---
+++ Q0599.java
@@ -0,0 +1,11 @@
+// 두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, 
+// flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+
+// 성공
+
+class Solution {
+    public int solution(int a, int b, boolean flag) {
+        if(flag) return a+b;
+        return a-b;
+    }
+}
