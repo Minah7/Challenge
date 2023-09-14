fb0f529b3360df7374ada9477463740fcf0b0b75
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-15 00:33:39 +0900

    n의 배수 23.09.15

---
+++ Q0594.java
@@ -0,0 +1,11 @@
+// 정수 num과 n이 매개 변수로 주어질 때, 
+// num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+
+// 성공
+
+class Solution {
+    public int solution(int num, int n) {
+        if(num%n == 0) return 1;
+        return 0;
+    }
+}
