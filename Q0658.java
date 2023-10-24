d4f840660d90b9acb47a7479d963b35f31350c29
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-10-24 23:59:56 +0900

    2027. 대각선 출력하기 23.10.24

---
+++ Q0658.java
@@ -0,0 +1,12 @@
+//다른 방법으로 구현하고 싶었지만 그러지 못했다..
+//성공
+public class 대각선_출력하기 {
+	public static void main(String[] args) {
+		System.out.println("#++++");
+		System.out.println("+#+++");
+		System.out.println("++#++");
+		System.out.println("+++#+");
+		System.out.println("++++#");
+		
+	}
+}
