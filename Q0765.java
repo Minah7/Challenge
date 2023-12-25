23c835d8e1f26edec62a3e807d2ba2e098cdf1a4
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-12-26 03:57:01 +0900

    P덧칠하기 23.12.26

--- Q0765.java
+++ Q0765.java
@@ -1,4 +1,21 @@
 // 문제링크: https://school.programmers.co.kr/learn/courses/30/lessons/161989
+// 두번째 시도 성공
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        // 구역은 1번부터 시작하니 0으로 초기화.
+        // 페인트 칠한 마지막 구역표시를 위한 변수.
+        int paint = 0;
+        for(int s = 0; s < section.length; s++) {
+            if(section[s] >= paint) {
+                paint = section[s]+m;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
+
 // 실패. 테케 80% 성공
 
 class Solution {
